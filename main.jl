d783347d6cce5097a9a0a5da128f47e1bec48847
pavelputsche <115639389+pavelputsche@users.noreply.github.com>
2022-10-17 12:50:18 +0000

    Initial commit

---
+++ main.jl
@@ -0,0 +1,17 @@
+
+function unterteilung(gesamtMenge)
+
+  teil1=gesamtMenge*.75
+  teil2=gesamtMenge*.15
+  teil3=gesamtMenge*.10
+
+  teil1,teil2,teil3
+end
+
+ 
+
+erg1, erg2, erg3 = unterteilung(20)
+
+println(erg1)
+println(erg2)
+println(erg3)
